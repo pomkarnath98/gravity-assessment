d65c4bc8c9bb15e1728e024ad80f43cc664b9255
Omkarnath Parida <omkarnath.parida@yocket.in>
2025-05-20 15:11:44 +0530

    📦 NEW: Added DSA solution 2

---
+++ DSA/solution2.js
@@ -0,0 +1,19 @@
+function solution2(nums, target) {
+    if (!Array.isArray(nums) || typeof target !== 'number') {
+        throw new Error("Invalid input");
+    }
+
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+
+    throw new Error("No two sum solution");
+}
+
+module.exports = solution2;
